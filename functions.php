aa0e92d44fb62414d10953fd468c320f431a1c91
NIKOLAKAKIS-PC\nikol <nikolakakis.manolis@gmail.com>
2025-06-12 20:55:16 +0300

    alpha16.2

--- functions.php
+++ functions.php
@@ -43,6 +43,9 @@ require_once GEOTOUR_THEME_DIR . '/includes/shortcodes/content-gallery.php';
 // Map functionality
 require_once GEOTOUR_THEME_DIR . '/includes/maps/icon-management.php';
 
+// REST API endpoints
+require_once GEOTOUR_THEME_DIR . '/includes/api/spatial-info-v2.php';
+
 // Custom hooks and filters (Uncomment when ready)
 // require_once GEOTOUR_THEME_DIR . '/includes/hooks.php'; // Note: your file is named hook.php, consider renaming to hooks.php for consistency or update here
 
