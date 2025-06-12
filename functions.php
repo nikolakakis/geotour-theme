d8e60f0237da925efbbcbd2056b26ca06e5f308a
NIKOLAKAKIS-PC\nikol <nikolakakis.manolis@gmail.com>
2025-06-12 21:48:45 +0300

    beta2

--- functions.php
+++ functions.php
@@ -71,3 +71,52 @@ add_action('after_setup_theme', function() {
         'primary' => __('Primary Menu', 'geotour-theme'),
     ]);
 });
+
+/**
+ * Disable comments and trackbacks completely
+ */
+function geotour_disable_comments_completely() {
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+}
+add_action('admin_init', 'geotour_disable_comments_completely');
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page in menu
+add_action('admin_menu', function() {
+    remove_menu_page('edit-comments.php');
+});
+
+// Remove comments links from admin bar
+add_action('init', function() {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
+
+// Remove comments metabox from dashboard
+add_action('admin_init', function() {
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+});
+
+// Remove comments from REST API
+add_filter('rest_endpoints', function($endpoints) {
+    if (isset($endpoints['/wp/v2/comments'])) {
+        unset($endpoints['/wp/v2/comments']);
+    }
+    if (isset($endpoints['/wp/v2/comments/(?P<id>[\d]+)'])) {
+        unset($endpoints['/wp/v2/comments/(?P<id>[\d]+)']);
+    }
+    return $endpoints;
+});
