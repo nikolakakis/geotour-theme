51fad1da5638027d3467babef1d3326ffcf6e95c
NIKOLAKAKIS-PC\nikol <nikolakakis.manolis@gmail.com>
2025-07-03 21:57:34 +0300

    version1.2.1

--- template-parts/listing/details-sections.php
+++ template-parts/listing/details-sections.php
@@ -241,7 +241,6 @@ $section_class = $has_additional_info ? 'has-additional-info' : 'weather-only';
                                         </li>
                                     <?php endforeach; ?>
                                 </ul>
-                                <?php echo do_shortcode('[population_chart]'); ?>
                             </div>
                         <?php endif; ?>
                     </div>
