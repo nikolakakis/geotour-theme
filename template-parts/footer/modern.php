5e83bdaaef4f61cea44772b554c30f9e8af8454f
NIKOLAKAKIS-PC\nikol <nikolakakis.manolis@gmail.com>
2025-08-15 20:02:22 +0300

    Version1.4.4.1

--- template-parts/footer/modern.php
+++ template-parts/footer/modern.php
@@ -26,8 +26,10 @@ $is_english = ($current_lang === 'en');
                 </p>
                 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
                     <input type="text" name="s" placeholder="<?php echo $is_english ? 'Keyword to search...?' : 'Λέξη-κλειδί για αναζήτηση...?'; ?>">
-                    <button type="submit" class="search-submit" aria-label="<?php echo $is_english ? 'Search' : 'Αναζήτηση'; ?>">
-                        <i class="fas fa-search search-icon"></i>
+                    <button type="submit" class="search-submit" aria-label="<?php echo $is_english ? 'Search' : 'Αναζήτηση'; ?>" title="<?php echo $is_english ? 'Search the website' : 'Αναζήτηση στον ιστότοπο'; ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                        </svg>
                     </button>
                     <?php if (function_exists('pll_current_language')) : ?>
                         <input type="hidden" name="lang" value="<?php echo esc_attr($current_lang); ?>">
